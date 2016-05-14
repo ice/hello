4b3541273575eb207951bd8fef3623e6ab450399
Mariusz Łączak <mruz@poczta.onet.pl>
2016-05-14 07:59:31 +0200

    Index, fix the error 500 if sql server is not running

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ defined('__ROOT__') or
      */
     define('__ROOT__', dirname(__DIR__));
 
-// Load the bootstrap which return the MVC application
-$app = require_once __ROOT__ . '/App/Bootstrap.php';
-
 try {
+    // Load the bootstrap which return the MVC application
+    $app = require_once __ROOT__ . '/App/Bootstrap.php';
+
     // Handle a MVC request and display the HTTP response body
     echo $app->handle();
 } catch (Exception $e) {
